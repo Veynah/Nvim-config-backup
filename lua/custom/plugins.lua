1adb72385b63474b3288a768bcb48b665fb2a482
AdamTangiev <tangiev96@gmail.com>
2025-01-06 14:18:07 +0100

    Save config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -73,6 +73,10 @@ local plugins = {
     },
   },
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
